39ae8acab4cb81ec645a07c57ccd501382d56b82
David Shalshin <dshalshin20@gmmail.com>
2023-03-22 22:11:59 -0500

    pulled previous code, added new decoder

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def main():
                 print(encoder(password))
             else:
                 print('Please enter an eight digit password.')
+        if option == 2:
+            password = input('Please enter an eight digit number: ')
+            digits = len(password)
+
+            if digits == 8:
+                print(decoder(password))
+            else:
+                print('Please enter an eight digit password.')
         else:
             break
 
@@ -19,6 +27,14 @@ def encoder(password):
         encoding = (int(i) + 3) % 10
         epassword += str(encoding)
     return epassword
+
+
+def decoder(password):
+    dpassword = ''
+    for i in password:
+        encoding = (int(i) - 3) % 10
+        dpassword += str(encoding)
+    return dpassword
 
 
 if __name__ == "__main__":
